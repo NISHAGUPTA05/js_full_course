dc0ef43d544457581201e77d44b57da20b1b7f73
NISHA GUPTA <110088592+NISHAGUPTA05@users.noreply.github.com>
2025-03-01 08:31:57 +0000

    datetype

---
+++ .devcontainer/01_basics/02_datatype.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as never version
+// alert (3 + 3) // we are using nodejs, not browser
+
+console.log(3 +
+     3)// code readability should high
+
+console.log("nisha")
+
+let name = "nisha"
+let age = 21
+let isLoggedIn = true
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+    // symbol => unique
+
+console.log(typeof "nisha")
+
+console.log(typeof 18)
+console.log(typeof null)
+console.log(typeof undifind)
+
+
+
+
