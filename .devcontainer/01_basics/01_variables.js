d672264ed93c7720525d620b10ed36973d286f63
NISHA GUPTA <110088592+NISHAGUPTA05@users.noreply.github.com>
2025-03-01 07:46:28 +0000

    container config done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 1
+let accountEmail = "nisha7452@google.com"
+var accountPassword = "123"
+let accountCity = "mumbai"
+let accountState;
+
+
+// accountId = 123 // not allowed
+
+accountEmail = "agg452@google.com"
+accountPassword = "321"
+accountCity = "gujrat"
+accountState;
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountCity, accountState , accountPassword])
