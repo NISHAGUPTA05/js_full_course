4e31ca8365dc2ca8daf58d92463bb9c1df34fc79
NISHA GUPTA <110088592+NISHAGUPTA05@users.noreply.github.com>
2025-03-05 07:53:41 +0000

    Types of memory

---
+++ .devcontainer/01_basics/05_typesofmemorydatatypes.js
@@ -0,0 +1,42 @@
+// +++++++++++++++++++++++++++++++ Types of Datatypes +++++++++++++++++++++++++++++++++++ 
+
+// there are two types of data types
+
+// primitive datatype
+
+// primitive datatype devided into 7 types
+// examples: string, number, boolean, null, undefined, symbol, BigInt
+
+//Non-primitive (reference) datatype
+
+// Examples: [Array, object, function ]
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// +++++++++++++++++++++ Types of Memory ++++++++++++++++++++++++++++++++++
+
+// Stack(primitive), Heap(Non-primitive)
+
+let myInstragramname = "nishagupta"
+
+let anothername = myInstragramname
+anothername = "nishugupta"
+ console.log(anothername);
+ console.log(myInstragramname);
+
+//  Heap Example:
+
+
+let user = {
+    email:"ddjh@google.com",
+    upi:"vgd@yml"
+}
+
+let user1 = {
+    email:"nhfds@google.com",
+    upi:"vafdd@yml"
+}
+console.log(user);
+console.log(user1);
+
+
